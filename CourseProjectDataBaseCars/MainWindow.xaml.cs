88ff41ad794fd859ad779cf0c0279e182e2e006f
Ян Орешко <oreshko2001@gmail.com>
2021-03-09 20:04:14 +0300

    test

--- CourseProjectDataBaseCars/MainWindow.xaml.cs
+++ CourseProjectDataBaseCars/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace CourseProjectDataBaseCars
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TaskbarIcon mTray;
         public MainWindow()
         {
             InitializeComponent();
+            mTray = FindResource("MyTrayIcon") as TaskbarIcon;
         }
     }
 }
